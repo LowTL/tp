ec3ba21bd49aa0d39f0f51db0af0b69447896714
LowTL <e0957642@u.nus.edu>
2024-03-29 23:07:20 +0800

    Add AddCommandTest

---
+++ src/test/java/command/AddCommandTest.java
@@ -0,0 +1,27 @@
+package command;
+
+import exceptions.CommandFormatException;
+import item.Item;
+import itemlist.Itemlist;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class AddCommandTest {
+
+    @Test
+    public void addItemTest() throws CommandFormatException {
+        try {
+            Command addCommandTest1 = new AddCommand("testItem", 1, "EA",
+                    "NA", 1, 10);
+            Command addCommandTest2 = new AddCommand("testItem", 7, "EA",
+                    "NA", 1, 10);
+            addCommandTest1.execute();
+            assert(Itemlist.itemIsExist("testItem"));
+            addCommandTest2.execute();
+        } catch (CommandFormatException e) {
+            fail("Unable to add item.");
+        }
+    }
+
+}

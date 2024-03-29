26180e33296614390c31ca862e6ec949b1407458
Shu <e0957502@u.nus.edu>
2024-03-29 20:05:04 +0800

    Add Empty arraylist test case

--- src/test/java/ui/TextUiTest.java
+++ src/test/java/ui/TextUiTest.java
@@ -1,19 +1,14 @@
 package ui;
 
-import item.Item;
 import org.junit.jupiter.api.Test;
-
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertLinesMatch;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static ui.TextUi.getUserInput;
 import static ui.TextUi.showInventoryList;
 
@@ -44,6 +39,19 @@ public class TextUiTest {
         String line2 = "1. test 1";
         String line3 = "2. test 2";
         List<String> expectedOutput = Arrays.asList(line1, line2, line3);
+        assertLinesMatch(expectedOutput, Arrays.asList(output));
+    }
+
+    @Test
+    public <T> void testShowEmptyInventoryList() { //only test for empty arrayList as parser will trim away empty inputs
+        ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(outputStreamCaptor));
+        ArrayList<String> itemList = new ArrayList<>(); //empty arrayList
+        showInventoryList(itemList);
+        String[] output = outputStreamCaptor.toString().split("\\r?\\n");
+        String line = "There is nothing here! Time to spend some money and stock em up!";
+        List<String> expectedOutput = List.of(line);
         assertLinesMatch(expectedOutput, Arrays.asList(output));
     }
 

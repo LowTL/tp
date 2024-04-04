6718c732803ff5edfd6f30996e3702a8a3de9821
LowTL <e0957642@u.nus.edu>
2024-04-04 18:45:06 +0800

    Separate CashierCommands

---
+++ src/main/java/command/BestsellerCommand.java
@@ -0,0 +1,17 @@
+package command;
+
+import item.Item;
+import itemlist.Cashier;
+import parser.CommandType;
+import ui.TextUi;
+
+public class BestsellerCommand extends Command {
+
+
+    @Override
+    public void execute() {
+        Item bs = Cashier.getBestseller();
+        TextUi.replyToUser("The current best-selling item is " +
+                bs.getItemName());
+    }
+}

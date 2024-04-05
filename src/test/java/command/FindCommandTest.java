b1ac64c7bb691522bbd784b8899e1abdb38dcf71
LowTL <e0957642@u.nus.edu>
2024-04-05 13:46:56 +0800

    Fix test exception handling

--- src/test/java/command/FindCommandTest.java
+++ src/test/java/command/FindCommandTest.java
@@ -1,6 +1,7 @@
 package command;
 
 import exceptions.CommandFormatException;
+import exceptions.EmptyListException;
 import exceptions.InvalidDateException;
 import itemlist.Itemlist;
 import org.junit.jupiter.api.Test;
@@ -10,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 public class FindCommandTest {
 
     @Test
-    public void findCommandTest() throws CommandFormatException, InvalidDateException {
+    public void findCommandTest() throws CommandFormatException, InvalidDateException, EmptyListException {
         Command addCommandTest1 = new AddCommand("testItem", 1, "EA",
                 "NA", 1, 10);
         Command findCommand = new FindCommand("item","testItem");

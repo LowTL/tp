8a21b1ec4f39c60e6c0056a773e43e2a7459bad0
LowTL <e0957642@u.nus.edu>
2024-04-14 13:11:48 +0800

    Add ListCommandTests

--- src/test/java/command/ListCommandTest.java
+++ src/test/java/command/ListCommandTest.java
@@ -14,10 +14,14 @@ import org.junit.jupiter.api.Test;
 import promotion.Month;
 import promotion.Promotion;
 import promotion.Promotionlist;
+import storage.PromotionStorage;
+import storage.Storage;
+import storage.TransactionLogs;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -35,12 +39,16 @@ public class ListCommandTest extends Cashier {
         while (Cashier.getTransaction(0) != null) {
             transactions = new ArrayList<>();
         }
+        Storage.updateFile("", false);
+        PromotionStorage.updateFile("", false);
+        TransactionLogs.updateFile("", false);
     }
 
     //happy case: has item
     @Test
     public void listCommandTest_itemList_correct() {
-        Item test = new Item("testItem", 1, "ea", "NA", 1.00F, 2.00F);
+        Item test = new Item("testItem", 1, "ea", "NA",
+                1.00F, 2.00F);
         Itemlist.addItem(test);
         ListCommand listCommand1 = new ListCommand(Itemlist.getItems(), "NA", false);
         try {
@@ -56,10 +64,135 @@ public class ListCommandTest extends Cashier {
 
     }
 
+
+    //happy case: has item, filtered corrrectly
+    @Test
+    public void listCommandTest_itemList_correct2() {
+        Item test = new Item("testItem", 1, "ea", "test",
+                1.00F, 2.00F);
+        Itemlist.addItem(test);
+        ListCommand listCommand1 = new ListCommand(Itemlist.getItems(), "test", false);
+        try {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(outputStream));
+            listCommand1.execute();
+            String expected = "1. Item Index: 1. " + test + System.lineSeparator();
+            assertEquals(expected, outputStream.toString());
+        } catch (EmptyListException e) {
+            Assertions.fail("Unexpected EmptyListException thrown.");
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    //happy case: has item, filtered wrongly
+    @Test
+    public void listCommandTest_itemList_correct3() {
+        Item test = new Item("testItem", 1, "ea", "noCat",
+                1.00F, 2.00F);
+        Itemlist.addItem(test);
+        ListCommand listCommand1 = new ListCommand(Itemlist.getItems(), "test", false);
+        try {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(outputStream));
+            listCommand1.execute();
+            String expected = "No items were found within the category test." + System.lineSeparator();
+            assertEquals(expected, outputStream.toString());
+        } catch (EmptyListException e) {
+            Assertions.fail("Unexpected EmptyListException thrown.");
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    //happy case: has item, search for marked and is marked
+    @Test
+    public void listCommandTest_itemList_correct4() {
+        Item test = new Item("testItem", 1, "ea", "NA",
+                1.00F, 2.00F);
+        Itemlist.addItem(test);
+        Objects.requireNonNull(Itemlist.getItem(0)).mark();
+        ListCommand listCommand1 = new ListCommand(Itemlist.getItems(), "NA", true);
+        try {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(outputStream));
+            listCommand1.execute();
+            String expected = "1. Item Index: 1. " + test + System.lineSeparator();
+            assertEquals(expected, outputStream.toString());
+        } catch (EmptyListException e) {
+            Assertions.fail("Unexpected EmptyListException thrown.");
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    //Error: Filter for marked, but no items are marked
+    @Test
+    public void listCommandTest_itemList_correct5() {
+        Item test = new Item("testItem", 1, "ea", "NA",
+                1.00F, 2.00F);
+        Itemlist.addItem(test);
+        ListCommand listCommand1 = new ListCommand(Itemlist.getItems(), "NA", true);
+        try {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(outputStream));
+            listCommand1.execute();
+            String expected = "There are no marked items in your inventory list!" + System.lineSeparator();
+            assertEquals(expected, outputStream.toString());
+        } catch (EmptyListException e) {
+            Assertions.fail("Unexpected EmptyListException thrown.");
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    //Error: Filter for marked + cat, have result
+    @Test
+    public void listCommandTest_itemList_correct6() {
+        Item test = new Item("testItem", 1, "ea", "test",
+                1.00F, 2.00F);
+        Itemlist.addItem(test);
+        Objects.requireNonNull(Itemlist.getItem(0)).mark();
+        ListCommand listCommand1 = new ListCommand(Itemlist.getItems(), "test", true);
+        try {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(outputStream));
+            listCommand1.execute();
+            String expected = "1. Item Index: 1. " + test + System.lineSeparator();
+            assertEquals(expected, outputStream.toString());
+        } catch (EmptyListException e) {
+            Assertions.fail("Unexpected EmptyListException thrown.");
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    //Error: Filter for marked + cat, have no result
+    @Test
+    public void listCommandTest_itemList_correct7() {
+        Item test = new Item("testItem", 1, "ea", "test",
+                1.00F, 2.00F);
+        Itemlist.addItem(test);
+        ListCommand listCommand1 = new ListCommand(Itemlist.getItems(), "test", true);
+        try {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(outputStream));
+            listCommand1.execute();
+            String expected = "There are no marked items of category '" + "test"+
+                    "' in your inventory list!" + System.lineSeparator();
+            assertEquals(expected, outputStream.toString());
+        } catch (EmptyListException e) {
+            Assertions.fail("Unexpected EmptyListException thrown.");
+            throw new RuntimeException(e);
+        }
+
+    }
+
     //Happy case: has 1 transaction
     @Test
     public void listCommandTest_transactionList_correct() {
-        AddCommand addCommand = new AddCommand("testItem", 1, "ea", "NA", 1.00F, 2.00F);
+        AddCommand addCommand = new AddCommand("testItem", 1, "ea",
+                "NA", 1.00F, 2.00F);
         addCommand.execute();
         SellCommand sellCommand = new SellCommand("testItem", 1, 0);
         ListCommand listCommand1 = new ListCommand(transactions, "NA");
@@ -83,7 +216,8 @@ public class ListCommandTest extends Cashier {
     //happy case: filtered item successfully
     @Test
     public void listCommandTest_transactionList_correct2() {
-        AddCommand addCommand = new AddCommand("testItem", 1, "ea", "NA", 1.00F, 2.00F);
+        AddCommand addCommand = new AddCommand("testItem", 1, "ea",
+                "NA", 1.00F, 2.00F);
         addCommand.execute();
         SellCommand sellCommand = new SellCommand("testItem", 1, 0);
         try {
@@ -106,7 +240,8 @@ public class ListCommandTest extends Cashier {
 
     @Test
     public void listCommandTest_transactionList_correct3() {
-        AddCommand addCommand = new AddCommand("testItem", 1, "ea", "NA", 1.00F, 2.00F);
+        AddCommand addCommand = new AddCommand("testItem", 1, "ea",
+                "NA", 1.00F, 2.00F);
         addCommand.execute();
         SellCommand sellCommand = new SellCommand("testItem", 1, 0);
         ListCommand listCommand1 = new ListCommand(transactions, "failTest");
@@ -129,10 +264,12 @@ public class ListCommandTest extends Cashier {
 
     @Test
     public void listCommandTest_promotionList_correct() throws InvalidDateException, CommandFormatException {
-        AddCommand addCommand = new AddCommand("testItem", 1, "ea", "NA", 1.00F, 2.00F);
+        AddCommand addCommand = new AddCommand("testItem", 1, "ea",
+                "NA", 1.00F, 2.00F);
         addCommand.execute();
-        Command testPromo = new AddPromotionCommand("testItem", 0.30F, 2, Month.valueOf("APR"),
-                2024, 4, Month.valueOf("APR"), 2024, 0000, 1100);
+        Command testPromo = new AddPromotionCommand("testItem", 0.30F, 2,
+                Month.valueOf("APR"), 2024, 4, Month.valueOf("APR"),
+                2024, 0000, 1100);
         try {
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
             System.setOut(new PrintStream(outputStream));

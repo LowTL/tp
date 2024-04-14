e28e7480e1c8492c76a9f93526e35ff75358d4c3
Joellimjr <joellimjr12@gmail.com>
2024-04-15 03:32:57 +0800

    addcommand tests

--- src/test/java/command/AddCommandTest.java
+++ src/test/java/command/AddCommandTest.java
@@ -1,36 +1,63 @@
 package command;
 
-import exceptions.CommandFormatException;
-import exceptions.EmptyListException;
-import exceptions.InvalidDateException;
+import item.Item;
 import itemlist.Itemlist;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import promotion.Promotionlist;
 import storage.PromotionStorage;
 import storage.Storage;
 import storage.TransactionLogs;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 public class AddCommandTest {
 
-    @Test
-    public void addItemTest() {
-        try {
-            Command addCommandTest1 = new AddCommand("testItem", 1, "EA",
-                    "NA", 1, 10);
-            Command addCommandTest2 = new AddCommand("testItem", 7, "EA",
-                    "NA", 1, 10);
-            addCommandTest1.execute();
-            addCommandTest2.execute();
-        } catch (CommandFormatException e) {
-            fail("Unable to add item.");
-        } catch (InvalidDateException | EmptyListException e) {
-            throw new RuntimeException(e);
-        }
+
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
     }
 
+    @Test
+    public void testExecuteNewItemAddedSuccessfully() {
+        new AddCommand("TestItem", 10, "pcs", "TestCategory", 5.0f, 10.0f).execute();
+        String expectedOutput = "added: testitem (Qty: 10 pcs, Buy: $5.00, Sell: $10.00) to TestCategory" + System.lineSeparator();
+        assertEquals(expectedOutput, outputStreamCaptor.toString());
+        assertEquals(1, Itemlist.getItems().size());
+    }
+    @Test
+    public void testExecuteNewItemAddedWithDefaultCategory() {
+        new AddCommand("TestItem", 10, "pcs", "NA", 5.0f, 10.0f).execute();
+        String expectedOutput = "added: testitem (Qty: 10 pcs, Buy: $5.00, Sell: $10.00)" + System.lineSeparator();
+        assertEquals(expectedOutput, outputStreamCaptor.toString());
+        assertEquals(1, Itemlist.getItems().size());
+    }
+
+    @Test
+    public void testExecuteExistingItemEditedSuccessfully() {
+        new AddCommand("TestItem", 10, "pcs", "NA", 5.0f, 10.0f).execute();
+        new AddCommand("TestItem", 15, "pcs", "NA", 6.0f, 12.0f).execute();
+        String expectedOutput = "added: testitem (Qty: 10 pcs, Buy: $5.00, Sell: $10.00)" + System.lineSeparator() +
+                "Item already exists and item information has been updated" + System.lineSeparator() +
+                "\n" +
+                "Edited: " + System.lineSeparator() +
+                "Quantity of TestItem from 10 to 25" + System.lineSeparator() +
+                "Buy Price of TestItem from 5.0 to 6.00" + System.lineSeparator() +
+                "Sell Price of TestItem from 10.0 to 12.00" + System.lineSeparator() +
+                "End of Edits" + System.lineSeparator() + System.lineSeparator();
+        assertEquals(expectedOutput, outputStreamCaptor.toString());
+        assertEquals(1, Itemlist.getItems().size());
+    }
     @AfterEach
     void tearDown() {
         // This will be run after each test, cleaning up
